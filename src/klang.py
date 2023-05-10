af6990dd2e537b7a8d5232911072af99421ff332
skelton codes <87882202+skeltoncodez@users.noreply.github.com>
2023-05-10 12:03:05 +0000

    i updated the source to follow pylint

--- src/klang.py
+++ src/klang.py
@@ -1,4 +1,9 @@
-import sys, kpiler, kterpret, argparse, klinker, kpilerllvm
+"""k language main file"""
+import argparse
+import kpiler
+import kterpret
+import klinker
+import kpilerllvm
 parser = argparse.ArgumentParser(prog='klang', description='Basic operations for the k language')
 parser.add_argument('files', metavar='file', nargs='+', help='input file(s)')
 parser.add_argument('-c', '--compile', action='store_true', required=False)
@@ -10,28 +15,28 @@ args = parser.parse_args()
 if args.compile and args.link:
     out_file = args.output
     if args.output is None:
-        raise Exception('need output')
-    output = kpilerllvm.main(*args.files)
-    with open(out_file, 'w') as f:
+        raise ValueError('need output')
+    OUTPUT = kpilerllvm.main(*args.files)
+    with open(out_file, 'w', encoding='utf-8') as f:
         print(f'Outputting to {out_file}')
-        f.write(output)
+        f.write(OUTPUT)
 elif args.compile:
     out_file = args.output
     if args.output is None:
         out_file = args.files[0][:-2] + '.kasm'
-    with open(args.filename, 'r') as f:
-        output = kpiler.main(f.read())
-        with open(out_file, 'w') as f:
+    with open(args.filename, 'r', encoding='utf-8') as f:
+        OUTPUT = kpiler.main(f.read())
+        with open(out_file, 'w', encoding='utf-8') as f:
             print(f'Outputting to {out_file}')
-            f.write(output)
+            f.write(OUTPUT)
 elif args.run:
-    with open(args.filename, 'r') as f:
+    with open(args.filename, 'r', encoding='utf-8') as f:
         kterpret.main(f.read())
 elif args.link:
     out_file = args.output
     if args.output is None:
         out_file = args.filename[:-2]
-    with open(args.filename, 'r') as f:
+    with open(args.filename, 'r', encoding='utf-8') as f:
         klinker.main(f.read(), out_file)
 else:
     parser.print_help()
