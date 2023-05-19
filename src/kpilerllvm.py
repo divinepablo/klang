6450c819c0f60e6f5f776b3518b4121e815cafcf
skeltoncodez <skelton@skeltronix.xyz>
2023-05-19 00:51:09 -0400

    fixed function declarations and calls

--- src/kpilerllvm.py
+++ src/kpilerllvm.py
@@ -2,10 +2,10 @@ from typing import Dict, Union
 from kparser import KParser
 from klexer import KLexer
 from dataclasses import dataclass, field
-from shared import llvm_types
+from shared import llvm_types, py_to_llvm_types
 from llvmlite import ir
 from llvmlite import binding as llvm
-import os, subprocess, llvmlite, colorama
+import os, subprocess, llvmlite, colorama, traceback
 
 def hid(t):
     if isinstance(t, tuple):
@@ -46,7 +46,11 @@ class KPiler:
 
         func_def = FunctionDefinition(name, llvm_function=llvm_func)
         for i, hi in enumerate(args):
-            func_def.local_variables[hi[1]] = llvm_func.args[i]
+            name = hi[1]
+            local_var = builder.alloca(llvm_func.args[i].type, name=name)
+            builder.store(llvm_func.args[i], local_var)
+            func_def.local_variables[name] = local_var
+
 
         for inst in block:
             self.compile_instruction(func_def, inst, builder)
@@ -60,11 +64,7 @@ class KPiler:
         if isinstance(inst, tuple):
             opcode = inst[0]
             if opcode == 'var':
-                var_value = func.local_variables[inst[1]]
-                if isinstance(var_value, int):
-                    print('arg_index:', var_value)
-                    return func.llvm_function.args[var_value]
-                return builder.load(var_value)
+                return builder.load(func.local_variables[inst[1]])
 
             elif opcode == 'DECLARE':
                 if isinstance(inst[3], str):
@@ -73,13 +73,13 @@ class KPiler:
                     alloca = builder.alloca(var_type, name=inst[2])
                     
                     func.local_variables[inst[2]] = alloca
-                    builder.store(value, alloca)
+                    return builder.store(value, alloca)
                 else:
                     value = self.compile_instruction(func, inst[3], builder)
                     var_type = llvm_types[inst[1]]
                     alloca = builder.alloca(var_type, name=inst[2])
                     func.local_variables[inst[2]] = alloca
-                    builder.store(value, alloca)
+                    return builder.store(value, alloca)
             elif opcode == 'GT':
                 expression1 = self.compile_instruction(func, inst[1], builder)
                 expression2 = self.compile_instruction(func, inst[2], builder)
@@ -126,25 +126,26 @@ class KPiler:
                 return builder.icmp_signed('!=', expression1, expression2)
             elif opcode == 'IF':
                 if_cond = self.compile_instruction(func, inst[1], builder)
-                with builder.if_then(if_cond):# as bbend:
-                    builder2 = builder#ir.IRBuilder(block=bbend)
+                with builder.if_then(if_cond):
                     for inst2 in inst[2][1]:
-                        self.compile_instruction(func, inst2, builder2)
+                        self.compile_instruction(func, inst2, builder)
             elif opcode == 'CALL':
+                callee_func = self.module.get_global(inst[1])
                 args = []
                 for arg in inst[2]:
-                    args.append(self.compile_instruction(func, arg, builder))
-                builder.call(self.llvm_functions[inst[1]], args)
+                    if arg:
+                        args.append(self.compile_instruction(func, arg, builder))
+                return builder.call(callee_func, args)
             elif opcode == 'ASSIGN':
                 value = self.compile_instruction(func, inst[2], builder)
                 index = func.local_variables[inst[1]]
-                builder.store(value, index)
+                return builder.store(value, index)
             elif opcode == 'RETURN':
                 if inst[1] != 'void':
                     value = self.compile_instruction(func, inst[1], builder)
-                    builder.ret(value)
+                    return builder.ret(value)
                 else:
-                    builder.ret_void()
+                    return builder.ret_void()
             elif opcode == 'WHILE':
                 cond_block = func.llvm_function.append_basic_block('while.cond')
                 loop_block = func.llvm_function.append_basic_block('while.loop')
@@ -188,20 +189,20 @@ class KPiler:
             # Implement array handling using ir.Builder methods
             pass
         else:
-            return llvm_types['int'](inst)
+            for pytype, lltype in py_to_llvm_types.items():
+                if isinstance(inst, pytype):
+                    return lltype(inst)
+            raise Exception("unexpected type")
+            # return llvm_types['int'](inst)
 
     def compile_code(self, src: str, module: ir.Module = None):
         if module is not None:
             self.module = module
-        # for func_name, func in self.llvm_functions.items():
-            # self.llvm_functions2[func_name] = ir.Function(self.module, func.ftype, func_name)
-            # self.llvm_functions2[func_name].linkage = 'external'
         
         if not 'printf' in self.llvm_functions:
             func_type = ir.FunctionType(llvm_types['int'], [ir.IntType(8).as_pointer()], True)
             llvm_func = ir.Function(self.module, func_type, name='printf')
             self.llvm_functions['printf'] = llvm_func
-            # self.llvm_functions2['printf'] = llvm_func
             
         lexer = KLexer()
         parser = KParser()
@@ -221,26 +222,30 @@ class KPiler:
         hi = 0
         os.makedirs('build', exist_ok=True)
         _files = files
-        for file in _files:
-            hi += 1
-            print(f'Compiling {file} ({hi}/{len(_files)})', end='', flush=True)
-            with open(file, 'r') as f:
-                self.create_module(hi)
-                hi2 = self.compile_code(f.read())
-                print(f'\rSaving {file} ({hi}/{len(_files)})', end='', flush=True)
-                compiled = str(hi2)
+        try:
+            for file in _files:
+                hi += 1
+                print(f'Compiling {file} ({hi}/{len(_files)})', end='', flush=True)
+                with open(file, 'r') as f:
+                    self.create_module(hi)
+                    hi2 = self.compile_code(f.read())
+                    print(f'\rSaving {file} ({hi}/{len(_files)})', end='', flush=True)
+                    compiled = str(hi2)
 
-                with open('build/' + file.replace('/', '.') + '.ll', 'w', encoding='utf-8') as f:
-                    f.write(compiled)
-            
-            print(f'\r{colorama.Fore.LIGHTGREEN_EX}Compiled {file} ({hi}/{len(_files)}) {colorama.Fore.RESET}')
+                    with open('build/' + file.replace('/', '.') + '.ll', 'w', encoding='utf-8') as f:
+                        f.write(compiled)
+                
+                print(f'\r{colorama.Fore.LIGHTGREEN_EX}Compiled {file} ({hi}/{len(_files)}) {colorama.Fore.RESET}')
+        except Exception as e:
+            print(f'\r{colorama.Fore.RED}Failed to compile {file} ({hi}/{len(_files)}) {colorama.Fore.RESET}')
+            traceback.print_exc()
         subprocess.run(['llvm-link', *['build/' + file.replace('/', '.') + '.ll' for file in _files], '-o', 'build/output.ll.bc'], check=True)
         subprocess.run(['llvm-dis', 'build/output.ll.bc', '-o', 'build/output.ll'], check=True)
 
         with open('build/output.ll', 'r', encoding='utf-8') as f:
             contents = f.read()
-        # for f in os.listdir('build'):
-        #     os.remove('build/' + f)
+        for f in os.listdir('build'):
+            os.remove('build/' + f)
         
         return contents
 
