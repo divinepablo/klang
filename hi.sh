a3ba6ad8d738dd80188a1f02f3acf693e5580807
skeltoncodez <skelton@skeltronix.xyz>
2023-05-28 02:18:18 -0400

    finished pointers

--- hi.sh
+++ hi.sh
@@ -4,5 +4,6 @@ source .venv/bin/activate
 python3 src/klang.py -o ignored/output -c $@
 echo "output from program:"
 ./ignored/output
+echo Proccess ended with return code: $? 
 deactivate
 rm ./ignored/output
